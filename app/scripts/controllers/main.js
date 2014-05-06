f4d70ed15196f37ac94c88d61f196d719ac46ba7
stompuri <seppo.tompuri@gmail.com>
2014-05-06 17:35:07 +0300

    first draft from frontend - backend communication: add items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,25 @@
-var app = angular.module('portfolioApp')
+var app = angular.module('portfolioApp', ["firebase"]);
 
-app.factory('Test', function() {
-	var service = {};
+app.factory('Items', function($firebase) {
+	var itemsService = {};
+  var REF = new Firebase("https://burning-fire-6770.firebaseio.com/");
 
-	service.test = function() {
-            return "Testi!"
-    }
+	itemsService.getAll = function() {
+     return $firebase(REF)
+  }
 
-    return service;
+  return itemsService;
 });
 
-app.controller('MainCtrl', function ($scope, Test) {
-	
+app.controller('MainCtrl', function ($scope, Items) {
+    $scope.item = {}
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      Test.test()
-    ];
+    $scope.items = Items.getAll()
+
+    $scope.addItem = function(e) {
+      if (e.keyCode != 13) return;
+      $scope.items.$add({title: $scope.title, desc: $scope.desc});
+      $scope.desc = "";
+    };
+
   });
